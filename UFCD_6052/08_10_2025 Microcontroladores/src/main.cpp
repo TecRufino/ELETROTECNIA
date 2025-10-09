e81f4fb9ea09093fdc7f3896f7fffa1bd84408dd
TecRufino <igorrufino5@gmail.com>
2025-10-09 15:42:08 +0100

    Update main.cpp

--- UFCD_6052/08_10_2025 Microcontroladores/src/main.cpp
+++ UFCD_6052/08_10_2025 Microcontroladores/src/main.cpp
@@ -1,32 +1,35 @@
 #include <Arduino.h>
 
 unsigned long tempoatual = 0;
-unsigned long tempoanterior = 1000; // Inicializa com 1 segundo para evitar acionamento imediato
-int ledPin = 13;
-int led2 = 12;
+unsigned long tempoanterior = 1000; 
+
+int ledPin1 = 13;
+int ledPin2 = 12;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
   tempoatual = millis();
   Serial.println(tempoatual);
 
-  if (tempoatual - tempoanterior > 1000) { // 1 segundo
+  if (tempoatual - tempoanterior > 1000) { // 1s
     Serial.print("tempo atual: ");
     Serial.println(tempoatual);
 
     Serial.print("tempo anterior: ");
     Serial.println(tempoanterior);
 
-    if (digitalRead(ledPin) == LOW) {
-      digitalWrite(ledPin, HIGH); // Liga o LED
-    } else {
-      digitalWrite(ledPin, LOW); // Desliga o LED
-    }
+    // Alterna o estado do LED 1
+    digitalWrite(ledPin1, !digitalRead(ledPin1));
 
-    tempoanterior = millis(); // Atualiza o tempo anterior
+    // Alterna o estado do LED 2
+    digitalWrite(ledPin2, !digitalRead(ledPin2));
+
+    tempoanterior = millis(); // Atualiza o tempo
   }
 
   delay(100);
